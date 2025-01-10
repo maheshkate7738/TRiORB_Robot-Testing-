a5abcdbb7b466feb227817a85cbd118874b495d0
mahesh <mahesh.kate@emagegroup.com>
2025-01-10 21:41:11 +0800

    added demo 1 follower

---
+++ examples/move_adamDemo1.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from triorb_robot_lib import TriOrbController
+
+def main():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s  (%(filename)s:%(lineno)d) [%(levelname)s] %(message)s")
+    
+    # Step 1: Connect to the robot
+    device_path = "/dev/ttyACM0"
+    robot = TriOrbController(device_path)
+
+    robot.reset_origin()
+    robot.wakeup()
+    
+    # You can also add custom operations:
+    robot.move(x_vel=0, y_vel=-0.2, z_vel=0, desired_distance=0.9, axis='y')
+    robot.lift(1)  # Lift up
+    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=2.5, axis='y')
+    #robot.lift(-1)  # Lift down
+    robot.move(x_vel=0, y_vel=-0.2, z_vel=0, desired_distance=2.5, axis='y')
+    robot.lift(-1)  # Lift down
+    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=0.9, axis='y')
+
+
+
+
+    # Stop the robot at the end
+    robot.stop()
+    logging.info("Robot operations completed.")
+
+if __name__ == "__main__":
+    main()
