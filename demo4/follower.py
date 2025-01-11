2c41a1df1989613889e52f22a7d7c276ab6c9f78
Deepak Yadav <deepakkumaryadav.ait@gmail.com>
2025-01-12 06:30:02 +0800

    fghjk

--- demo4/follower.py
+++ demo4/follower.py
@@ -55,7 +55,7 @@ def main():
     logger.info("Starting movements...")
     robot.move(x_vel=0, y_vel=-0.2, z_vel=0, desired_distance=0.9, axis="y")
     robot.lift(1)  # Lift up
-    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=3.2, axis="y")
+    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=3.0, axis="y")
     robot.turn(desired_angle=1.57, direction='ccw')
     robot.move(x_vel=0.2, y_vel=0, z_vel=0, desired_distance=1.8, axis="x")
     robot.lift(-1)  # Lift down
