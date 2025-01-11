cb21c18780a27a87e870055be7c4b55567a577dd
Deepak Yadav <deepakkumaryadav.ait@gmail.com>
2025-01-12 05:36:43 +0800

    added demo 4

--- demo4/guider.py
+++ demo4/guider.py
@@ -53,11 +53,14 @@ def main():
     # You can also add custom operations:
     logger.info("Starting movements...")
     time.sleep(15)
-    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=0.5, axis='y')
+    robot.move(x_vel=0, y_vel=0.2, z_vel=0, desired_distance=0.75, axis='y')
     robot.turn(desired_angle=1.57, direction='cw')
-    robot.move(x_vel=0.2, y_vel=0, z_vel=0, desired_distance=2.0, axis="x")
+    robot.move(x_vel=0.2, y_vel=0, z_vel=0, desired_distance=2.2, axis="x")
+    robot.turn(desired_angle=3.14, direction='cw')
+    time.sleep(5)
+    robot.move(x_vel=-0.2, y_vel=0.0, z_vel=0, desired_distance=2.2, axis="x")
     robot.turn(desired_angle=1.57, direction='cw')
-    robot.move(x_vel=0, y_vel=-0.2, z_vel=0, desired_distance=1.6, axis="y")
+    robot.move(x_vel=0, y_vel=-0.2, z_vel=0, desired_distance=0.75, axis='y')
     robot.get_pose()
 
     # Stop the robot at the end
